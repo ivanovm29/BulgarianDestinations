909522fbc145562d84e591d8eb79652bc9cd1f40
ivanov.m29@students.softuni.bg <ivanov.m29@students.softuni.bg>
2024-12-04 13:07:47 +0200

    Database added.

--- BulgarianDestinations.Infrastructure/Data/ApplicationDbContext.cs
+++ BulgarianDestinations.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulgarianDestinations.Data
+namespace BulgarianDestinations.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- BulgarianDestinations/Extensions/ServiceCollectionExtension.cs
+++ BulgarianDestinations/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,4 @@
-﻿using BulgarianDestinations.Data;
+﻿using BulgarianDestinations.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
