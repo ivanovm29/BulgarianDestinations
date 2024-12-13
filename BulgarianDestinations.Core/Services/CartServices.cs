a5158708b86e2c29ebea51a926c370a5157b277a
ivanov.m29@students.softuni.bg <ivanov.m29@students.softuni.bg>
2024-12-13 20:30:45 +0200

    do not add order when is no articuls

--- BulgarianDestinations.Core/Services/CartServices.cs
+++ BulgarianDestinations.Core/Services/CartServices.cs
@@ -19,7 +19,7 @@ namespace BulgarianDestinations.Core.Services
             return await repository.All<ArticulPerson>()
                 .OrderByDescending(d => d.ArticulId)
                 .Select(d => new ArticulViewModel()
-                    {
+                {
                     Id = d.Articul.Id,
                     Name = d.Articul.Name,
                     Description = d.Articul.Description,
@@ -34,9 +34,9 @@ namespace BulgarianDestinations.Core.Services
         {
             decimal totalPrice = 0;
 
-           var collection = await repository.AllReadOnly<ArticulPerson>().
-                Where(p => p.PersonId == personId)
-                .ToListAsync();
+            var collection = await repository.AllReadOnly<ArticulPerson>().
+                 Where(p => p.PersonId == personId)
+                 .ToListAsync();
             foreach (var item in collection)
             {
                 var articul = await repository.GetById<Articul>(item.ArticulId);
@@ -57,22 +57,26 @@ namespace BulgarianDestinations.Core.Services
             var articulsPersons = await repository.All<ArticulPerson>().Where(p => p.PersonId == personId).ToListAsync();
             var articuls = new List<Articul>();
             decimal totalPrice = 0;
-            foreach (var item in articulsPersons)
+            if (articulsPersons.Any())
             {
-                decimal price = repository.GetById<Articul>(item.ArticulId).Result.Price;
-                totalPrice += price;
-                articuls.Add(item.Articul);
-                await repository.DeleteSingleObjectAsync<ArticulPerson>(item);
+                foreach (var item in articulsPersons)
+                {
+                    decimal price = repository.GetById<Articul>(item.ArticulId).Result.Price;
+                    totalPrice += price;
+                    articuls.Add(item.Articul);
+                    await repository.DeleteSingleObjectAsync<ArticulPerson>(item);
+                }
+
+                await repository.AddAsync<Order>(new Order()
+                {
+                    PersonId = personId,
+                    Articuls = articuls,
+                    TotalPrice = totalPrice,
+                });
+
+                await repository.SaveChangesAsync();
             }
-
-            await repository.AddAsync<Order>(new Order()
-            {
-                PersonId = personId,
-                Articuls = articuls,
-                TotalPrice = totalPrice,
-            }) ;
-
-            await repository.SaveChangesAsync();
+    
         }
     }
 }
